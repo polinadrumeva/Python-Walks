8ee0f58d7d2407bba267cbc213cc0fb55d671b70
Polinadrumeva <polina9230@gmail.com>
2025-05-29 12:58:03 +0300

    Is prime exercise

---
+++ IsPrime/IsPrime.py
@@ -0,0 +1,25 @@
+﻿
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+def main():
+    try:
+        number = int(input("Въведете цяло число: "))
+        if is_prime(number):
+            print(f"{number} е просто число.")
+        else:
+            print(f"{number} не е просто число.")
+    except ValueError:
+        print("Грешка: моля, въведете валидно цяло число.")
+
+if __name__ == "__main__":
+    main()
